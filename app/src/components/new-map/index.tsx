0debc5153bcd23d7392995a636c53cd5daf21486
Guilherme Valença <guilherm3vrp@gmail.com>
2025-10-27 20:17:43 -0300

    feat: agora o formulário e o mapa não aparecem na mesma tela

--- app/src/components/new-map/index.tsx
+++ app/src/components/new-map/index.tsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import { useNewMap } from "../../hooks";
 import { CreateMap } from "./create-map";
 import { RenderMap } from "./render-map";
 
 export function NewMap() {
   const { map, player, refresh, nextMove } = useNewMap();
+  const [showForm, setShowForm] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  async function startGame() {
+    refresh();
+    setIsPlaying(true);
+    setShowForm(false);
+  }
+
+  async function newGame() {
+    setShowForm(true);
+    setIsPlaying(false);
+  }
+
   return (
     <>
-      <CreateMap getMap={() => refresh()} />
-      {map && <RenderMap map={map} player={player} nextMove={nextMove} />}
+      <button onClick={() => newGame()}>Iniciar Jogo</button>
+      {showForm && <CreateMap getMap={() => startGame()} />}
+      {map && isPlaying && (
+        <RenderMap map={map} player={player} nextMove={nextMove} />
+      )}
     </>
   );
 }
